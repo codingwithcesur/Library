de110fa93b5977b9746a2d87fc758dda5b6aacc1
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-07-06 16:48:47 +0300

    add book constructor

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 function addBookToLibrary() {}
