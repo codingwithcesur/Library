0c85d8fc7d79fc7530b8566508fbf19371499814
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-07-07 15:38:39 +0300

    add a function for displaying books

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
-// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
-// for (let i = 0; i < myLibrary.length; i++) {
-//   console.log(myLibrary[i]);
-// }
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+  }
+}
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", 423, true);
+addBookToLibrary("The Two Towers", "J.R.R. Tolkien", 352, true);
+displayBooks();
