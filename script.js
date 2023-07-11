d87aa1b8f2a39b115536f97126cebc3889ec2638
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-07-11 19:33:19 +0300

    add newbook button with form as an input

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let myLibrary = [];
+const addBookBtn = document.querySelector(".add-book-btn");
+const addBookForm = document.querySelector(".add-book-form");
+const submitBtn = document.querySelector("#submit-btn");
+
+addBookBtn.addEventListener("click", createBook);
+submitBtn.addEventListener("click", submitBook);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,35 +15,49 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
+  displayBook();
 }
 
-function displayBooks() {
+function toggleInvisibility() {
+  addBookBtn.classList.toggle("invisible");
+  addBookForm.classList.toggle("invisible");
+}
+function displayBook() {
+  let book = document.createElement("div"),
+    bookTitle = document.createElement("h2"),
+    bookAuthor = document.createElement("h3"),
+    bookPages = document.createElement("h3"),
+    bookRead = document.createElement("h3");
+
+  book.classList.add("book");
+  bookTitle.classList.add("book-title");
+  bookAuthor.classList.add("book-author");
+  bookPages.classList.add("book-pages");
+  bookRead.classList.add("book-read");
+
+  document.querySelector(".book-container").appendChild(book);
+  book.appendChild(bookTitle);
+  book.appendChild(bookAuthor);
+  book.appendChild(bookPages);
+  book.appendChild(bookRead);
   for (let i = 0; i < myLibrary.length; i++) {
-    let book = document.createElement("div");
-    let bookTitle = document.createElement("h2");
-    let bookAuthor = document.createElement("h3");
-    let bookPages = document.createElement("h3");
-    let bookRead = document.createElement("h3");
-
-    book.classList.add("book");
-    bookTitle.classList.add("book-title");
-    bookAuthor.classList.add("book-author");
-    bookPages.classList.add("book-pages");
-    bookRead.classList.add("book-read");
-
-    document.querySelector(".book-container").appendChild(book);
-    book.appendChild(bookTitle);
-    book.appendChild(bookAuthor);
-    book.appendChild(bookPages);
-    book.appendChild(bookRead);
-
     bookTitle.textContent = myLibrary[i].title;
     bookAuthor.textContent = `By : ${myLibrary[i].author}`;
     bookPages.textContent = `${myLibrary[i].pages} pages`;
     bookRead.textContent = `Did read : ${myLibrary[i].read}`;
   }
 }
-// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
-// addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", 423, true);
-// addBookToLibrary("The Two Towers", "J.R.R. Tolkien", 352, true);
-// displayBooks();
+
+function createBook() {
+  toggleInvisibility();
+}
+
+function submitBook() {
+  toggleInvisibility();
+  let title = document.querySelector("#title").value,
+    author = document.querySelector("#author").value,
+    pages = document.querySelector("#pages").value,
+    read = document.querySelector("#read").value;
+
+  addBookToLibrary(title, author, pages, read);
+}
